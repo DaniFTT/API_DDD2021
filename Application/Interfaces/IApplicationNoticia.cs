5b58ef058954b294e422cee7f3920c6061d547fc
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-11-05 20:00:40 -0300

    Criado a interface de aplicação Generica, aplicação noticia e usuario

---
+++ Application/Interfaces/IApplicationNoticia.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces.Generics;
+using Entidades.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationNoticia : IApplicationGeneric<Noticia>
+    {
+        Task AdicionaNoticia(Noticia noticia);
+        Task AtualizaNoticia(Noticia noticia);
+        Task<List<Noticia>> ListarNoticiasAtivas();
+        Task<List<Noticia>> ListarNoticiasInativas();
+    }
+}
