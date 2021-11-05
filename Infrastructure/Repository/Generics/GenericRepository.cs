240e3370ae8ad3011ad0b9b5633108858a443afc
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-11-04 22:52:38 -0300

    Criado repositorio generico com metodos crud para operacoes no banco

---
+++ Infrastructure/Repository/Generics/GenericRepository.cs
@@ -0,0 +1,88 @@
+﻿using Domain.Interfaces.Generics;
+using Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Generics
+{
+    public class GenericRepository<T> : IGenerics<T>, IDisposable where T : class
+    {
+        protected readonly DbContextOptions<Context> _optionsBuilder;
+        public GenericRepository()
+        {
+            _optionsBuilder = new DbContextOptions<Context>();
+        }
+
+        public async Task Add(T Object)
+        {
+            using (var data = new DbContext(_optionsBuilder))
+            {
+                await data.Set<T>().AddAsync(Object);
+                await data.SaveChangesAsync();
+            }
+        }
+        public async Task Update(T Object)
+        {
+            using (var data = new DbContext(_optionsBuilder))
+            {
+                data.Set<T>().Update(Object);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T Object)
+        {
+            using (var data = new DbContext(_optionsBuilder))
+            {
+                data.Set<T>().Remove(Object);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task<T> GetById(int Id)
+        {
+            using (var data = new DbContext(_optionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(Id);
+            }
+        }
+
+        public async Task<List<T>> List()
+        {
+            using (var data = new DbContext(_optionsBuilder))
+            {
+                return await data.Set<T>().AsNoTracking().ToListAsync();
+            }
+        }
+
+        #region IDisposable
+        private bool disposed = false;
+        private SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+            }
+
+            disposed = true;
+            
+        }
+        #endregion
+    }
+}
