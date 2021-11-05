ecc7b53769e8f9f96025885a78c1db68db123e6a
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-11-04 22:53:27 -0300

    Desenvolvido os repositorios para noticias e usuario, e implementado suas respectivas interfaces

---
+++ Infrastructure/Repository/UsuarioRepository.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Interfaces;
+using Entidades.Entities;
+using Infrastructure.Configurations;
+using Infrastructure.Repository.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    class UsuarioRepository : GenericRepository<ApplicationUser>, IUsuario
+    {
+        public async Task<bool> AdicionaUsuario(string email, string senha, int idade, string celular)
+        {
+            try
+            {
+                using (var data = new Context(_optionsBuilder))
+                {
+                    await data.ApplicationUser.AddAsync(
+                        new ApplicationUser 
+                        { 
+                            Email = email,
+                            PasswordHash = senha,
+                            Idade = idade,
+                            Celular = celular
+                        });
+
+                    await data.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
