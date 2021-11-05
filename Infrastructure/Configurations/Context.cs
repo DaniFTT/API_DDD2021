0916a87c226786693b4af8a354c87a54f85f6036
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-11-04 22:52:10 -0300

    Ajustado o namespace da classe Context para evitar confusão com a classe DbContext do entity

---
+++ Infrastructure/Configurations/Context.cs
@@ -0,0 +1,45 @@
+﻿using Entidades.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class Context : IdentityDbContext<ApplicationUser>
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Noticia> Noticia { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringDeConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringDeConexao()
+        {
+            string strncon = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=API_DDD_2021;Integrated Security=False;User ID=sa;Password=61403641;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            return strncon;
+        }
+    }
+}
