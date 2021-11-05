77a166abc0a854ce568ed4de61f93a848f4cc3a4
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-11-04 21:23:21 -0300

    Desenvolvida o service de Noticias, que implementa sua interface, e utilizado a logica de validação da Noticia

---
+++ Domain/Services/NoticiaServices.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Interfaces;
+using Domain.Interfaces.ServiceInterfaces;
+using Entidades.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class NoticiaServices : INoticiaService
+    {
+        private readonly INoticia _INoticia;
+
+        public NoticiaServices(INoticia iNoticia)
+        {
+            _INoticia = iNoticia;
+        }
+
+        public async Task AdicionaNoticia(Noticia noticia)
+        {
+            var validarTitulo = noticia.ValidarPropriedadeString(noticia.Titulo, "Titulo");
+            var validarInformacoes = noticia.ValidarPropriedadeString(noticia.Informacao, "Informacao");
+
+            if(validarTitulo && validarInformacoes)
+            {
+                noticia.DataAlteracao = DateTime.Now;
+                noticia.DataCadastro = DateTime.Now;
+                noticia.Ativo = true;
+
+                await _INoticia.Add(noticia);
+            }
+        }
+
+        public async Task AtualizaNoticia(Noticia noticia)
+        {
+            var validarTitulo = noticia.ValidarPropriedadeString(noticia.Titulo, "Titulo");
+            var validarInformacoes = noticia.ValidarPropriedadeString(noticia.Informacao, "Informacao");
+
+            if (validarTitulo && validarInformacoes)
+            {
+                noticia.DataAlteracao = DateTime.Now;
+                noticia.Ativo = true;
+
+                await _INoticia.Update(noticia);
+            }
+        }
+
+        public async Task<List<Noticia>> ListarNoticiasAtivas()
+        {
+            return await _INoticia.ListarNoticias(n => n.Ativo);
+        }
+    }
+}
