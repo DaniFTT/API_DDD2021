f343a8fc923aa3996e9b2185ce34fa3c4e8b8091
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-11-05 23:16:42 -0300

    Removido e reinserido projeto de testes por conta de bug

---
+++ Tests/UnitTestPortalDeNoticias.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTestPortalDeNoticias
+    {
+        public async Task AddNoticiaComSucesso()
+        {
+
+        }
+    }
+}
