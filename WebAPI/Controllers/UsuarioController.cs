cd2fdc915f1f9c4681398bfe347d5afa7e4e1a23
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-11-08 21:22:36 -0300

    Ajustado o metodo de criação do token JWT

--- WebAPI/Controllers/UsuarioController.cs
+++ WebAPI/Controllers/UsuarioController.cs
@@ -6,9 +6,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using WebAPI.Models;
@@ -16,24 +20,27 @@ using WebAPI.Token;
 
 namespace WebAPI.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private IConfiguration _config;
         private readonly IApplicationUsuario _applicationUsuario;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public UsuarioController(IApplicationUsuario applicationUsuario, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        public UsuarioController(IApplicationUsuario applicationUsuario, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration Configuration)
         {
             _applicationUsuario = applicationUsuario;
             _userManager = userManager;
             _signInManager = signInManager;
+            _config = Configuration;
         }
 
         [AllowAnonymous]
         [Produces("application/json")]
-        [HttpPost("api/CriaToken")]
-        public async Task<IActionResult> CriaToken([FromBody] Login login)
+        [HttpPost("/api/CriaToken")]
+        public async Task<IActionResult> CriaToken([FromBody] LoginModel login)
         {
             if (string.IsNullOrWhiteSpace(login.Email) || string.IsNullOrWhiteSpace(login.Senha))
                 return Unauthorized();
@@ -41,16 +48,24 @@ namespace WebAPI.Controllers
             var result = await _applicationUsuario.ExisteUsuario(login.Email, login.Senha);
             if (result)
             {
-                var token = new TokenJWTBuilder()
-                    .AddSecurityKey(JwtSecurityKey.Create("Secret_Key-1235678"))
-                    .AddSubject("Empresa - Daniel Santos")
-                    .AddIssuer("Teste.Security.Bearer")
-                    .AddAudience("Teste.Security.Bearer")
-                    .AddClaim("UsuarioAPINumero", "1")
-                    .AddExpiry(5)
-                    .Builder();
+                var userId = await _applicationUsuario.RetornaIdUsuario(login.Email);
+                var claims = new List<Claim>();
+                claims.Add(new Claim("UserId", userId));
 
-                return Ok(token.value);
+                var _secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+                var _issuer = _config["Jwt:Issuer"];
+                var _audience = _config["Jwt:Audience"];
+                var signinCredentials = new SigningCredentials(_secretKey, SecurityAlgorithms.HmacSha256);
+                var tokeOptions = new JwtSecurityToken(
+                    issuer: _issuer,
+                    audience: _audience,
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(60),
+                    signingCredentials: signinCredentials);
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+
+                return Ok(tokenString);
             }
             else
             {
@@ -60,18 +75,18 @@ namespace WebAPI.Controllers
 
         [AllowAnonymous]
         [Produces("application/json")]
-        [HttpPost("api/AdicionaUsuario")]
-        public async Task<IActionResult> AdicionaUsuario([FromBody] Login login)
+        [HttpPost("/api/AdicionaUsuario")]
+        public async Task<IActionResult> AdicionaUsuario([FromBody] RegisterModel register)
         {
-            if (string.IsNullOrWhiteSpace(login.Email) || string.IsNullOrWhiteSpace(login.Senha) || string.IsNullOrWhiteSpace(login.Celular) || login.Idade < 0)
+            if (string.IsNullOrWhiteSpace(register.Email) || string.IsNullOrWhiteSpace(register.Senha) || string.IsNullOrWhiteSpace(register.Celular) || register.Idade < 0)
                 return BadRequest("Faltam alguns dados!");
 
-            var userExists = await _applicationUsuario.ExisteUsuario(login.Email, login.Senha);
+            var userExists = await _applicationUsuario.ExisteUsuario(register.Email, register.Senha);
 
             if (!userExists)
             {
                 var result = await
-                    _applicationUsuario.AdicionaUsuario(login.Email, login.Senha, login.Idade, login.Celular);
+                    _applicationUsuario.AdicionaUsuario(register.Email, register.Senha, register.Idade, register.Celular);
 
                 if (result)
                     return Ok("Usuário Adicionado com Sucesso!");
@@ -85,8 +100,8 @@ namespace WebAPI.Controllers
 
         [AllowAnonymous]
         [Produces("application/json")]
-        [HttpPost("api/CriaTokenIdentity")]
-        public async Task<IActionResult> CriaTokenIdentity([FromBody] Login login)
+        [HttpPost("/api/CriaTokenIdentity")]
+        public async Task<IActionResult> CriaTokenIdentity([FromBody] LoginModel login)
         {
             if (string.IsNullOrWhiteSpace(login.Email) || string.IsNullOrWhiteSpace(login.Senha))
                 return Unauthorized();
@@ -96,16 +111,24 @@ namespace WebAPI.Controllers
 
             if (result.Succeeded)
             {
-                var token = new TokenJWTBuilder()
-                   .AddSecurityKey(JwtSecurityKey.Create("Secret_Key-1235678"))
-                   .AddSubject("Empresa - Daniel Santos")
-                   .AddIssuer("Teste.Security.Bearer")
-                   .AddAudience("Teste.Security.Bearer")
-                   .AddClaim("UsuarioAPINumero", "1")
-                   .AddExpiry(5)
-                   .Builder();
+                var userId = await _applicationUsuario.RetornaIdUsuario(login.Email);
+                var claims = new List<Claim>();
+                claims.Add(new Claim("UserId", userId));
 
-                return Ok(token.value);
+                var _secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+                var _issuer = _config["Jwt:Issuer"];
+                var _audience = _config["Jwt:Audience"];
+                var signinCredentials = new SigningCredentials(_secretKey, SecurityAlgorithms.HmacSha256);
+                var tokeOptions = new JwtSecurityToken(
+                    issuer: _issuer,
+                    audience: _audience,
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(60),
+                    signingCredentials: signinCredentials);
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+
+                return Ok(tokenString);
             }
             else
             {
@@ -115,22 +138,22 @@ namespace WebAPI.Controllers
 
         [AllowAnonymous]
         [Produces("application/json")]
-        [HttpPost("api/AdicionaUsuarioIdentity")]
-        public async Task<IActionResult> AdicionaUsuarioIdentity([FromBody] Login login)
+        [HttpPost("/api/AdicionaUsuarioIdentity")]
+        public async Task<IActionResult> AdicionaUsuarioIdentity([FromBody] RegisterModel register)
         {
-            if (string.IsNullOrWhiteSpace(login.Email) || string.IsNullOrWhiteSpace(login.Senha) || string.IsNullOrWhiteSpace(login.Celular) || login.Idade < 0)
+            if (string.IsNullOrWhiteSpace(register.Email) || string.IsNullOrWhiteSpace(register.Senha) || string.IsNullOrWhiteSpace(register.Celular) || register.Idade < 0)
                 return BadRequest("Faltam alguns dados!");
 
             var user = new ApplicationUser
             {
-                UserName = login.Email,
-                Email = login.Email,
-                Celular = login.Celular,
-                Idade = login.Idade,
+                UserName = register.Email,
+                Email = register.Email,
+                Celular = register.Celular,
+                Idade = register.Idade,
                 Tipo = TipoUsuario.Comum
             };
 
-            var result = await _userManager.CreateAsync(user, login.Senha);
+            var result = await _userManager.CreateAsync(user, register.Senha);
 
             if (result.Errors.Any())
                 return BadRequest(result.Errors);
